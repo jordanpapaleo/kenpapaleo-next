f23e8e112ab43ff56110ecb3de76fb469e0f96f4
Jordan Papaleo <papaleowebdev@gmail.com>
2024-03-23 15:43:06 -0700

    Landing page complete

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,87 @@
 import type { Config } from "tailwindcss";
+import plugin from 'tailwindcss/plugin';
+
+const CARBON = '#262626'
+const SLATE = '#292929'
+const AMBER = '#E77E0D'
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontFamily: {
+        DEFAULT: ['Bodoni Moda', 'serif'],
+        sans: [],
+        serif: ['Helvetica Neue', 'Arial', 'sans-serif'],
+      },
+      fontSize: {
+        p: ['1.25rem', {
+          fontWeight: 400,
+          letterSpacing: '0.36px',
+          lineHeight: '1.575'
+        }]
+      },
+      tracking: {
+        widest: '0.14em'
+      },
+      colors: {
+        amber: {
+          DEFAULT: AMBER,
+        },
+        slate: {
+          DEFAULT: SLATE,
+        },
+        carbon: {
+          DEFAULT: CARBON,
+        },
+        primary: AMBER,
+        content: {
+          DEFAULT: SLATE,
+          dark: CARBON
+        }
       },
     },
   },
-  plugins: [],
+  // https://tailwindcss.com/docs/plugins
+  plugins: [
+    plugin(({ addComponents }) => {
+      addComponents({
+        '.h1': {
+          fontFamily: 'Helvetica Neue',
+          fontSize: '1.4rem',
+          fontWeight: '500',
+          letterSpacing: '0.1875rem',
+          lineHeight: '1.45',
+          textTransform: 'uppercase',
+        },
+        '.hero': {
+          fontFamily: 'Alegreya',
+          fontSize: '9rem',
+          fontWeight: '400',
+          lineHeight: '1',
+          fontStyle: 'normal',
+          textTransform: 'none',
+          letterSpacing: '0em',
+        },
+        // '.btn-blue': {
+        //   backgroundColor: '#3490dc',
+        //   color: '#fff',
+        //   '&:hover': {
+        //     backgroundColor: '#2779bd'
+        //   },
+        // },
+        // '.btn-red': {
+        //   backgroundColor: '#e3342f',
+        //   color: '#fff',
+        //   '&:hover': {
+        //     backgroundColor: '#cc1f1a'
+        //   },
+        // },
+      })
+    })
+  ],
 };
 export default config;
